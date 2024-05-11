384d517671191dfc968748ed64a7ddf8666eb4e7
federicoalaniz <fedealaniz@gmail.com>
2024-05-11 01:24:30 -0300

    feat: booking-trip-type ver:1.00

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import Image from "next/image";
+//import { montserrat, montserrat } from "@/ui/fonts"
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ['latin']
+})
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Main Site</div>
+      <div className={montserrat.className}>Main Site</div>
       <a href="/login"> Login </a>
+      <a href="/booking"> Booking</a>
     </main>
   );
 }
+
+

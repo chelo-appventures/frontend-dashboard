7eace4527c3d4bd28f957c2f776b2cead142fa1e
chelo.appventures <chelo.appventures@gmail.com>
2024-05-21 15:08:52 +0000

    fix: counter

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -22,8 +22,9 @@ function AVCounterIcon({icon}: {icon: IconType}) {
       <Image 
         src={icons[icon as string]} 
         alt={icon as string} 
-        width={24} 
-        height={24}
+        width={32} 
+        height={32}
+        className=""
       />
     );
 }
@@ -33,20 +34,32 @@ export default function AVCounter(
     {icon: IconType, title: string, subtitle: string}
     ) {
     const [count, setCount] = useState(0);
-    const increment = () => setCount(count + 1);
-    const decrement = () => setCount(count - 1);
+    const increment = () => setCount(count + 1 > 0 ? count + 1 : 0);
+    const decrement = () => setCount(count - 1 > 0 ? count - 1 : 0);
 
     return (
-        <div className="flex flex-row items-center gap-2 bg-black">
+        <div className="flex flex-row items-center gap-2 border border-gray-200 p-2 rounded-lg">
             <AVCounterIcon icon={icon} />
             <div className="flex flex-col">
-                <p className="font-bold">{title}</p>
-                <p className="text-gray-500">{subtitle}</p>
+                <p className="text-smfont-bold">{title}</p>
+                <p className="text-xs text-gray-500">{subtitle}</p>
             </div>
-            <div>
-                <button className="rounded-full" onClick={()=> increment()}> + </button>       
-                <span>{count}</span>
-                <button className="rounded-full border border-gray-500 p-2" onClick={()=> decrement()}> - </button> 
+            <div className="flex flex-row items-center justify-center">
+                <button 
+                  className="m-2 w-7 h-7 rounded-full border border-red-500" 
+                  onClick={(e)=> {
+                    e.preventDefault();
+                    increment();
+                  }}
+                > + </button>       
+                <span className="w-3 text-center">{count}</span>
+                <button 
+                  className="m-2 w-7 h-7 rounded-full border border-red-500" 
+                  onClick={(e)=> {
+                    e.preventDefault();
+                    decrement();
+                  }}
+                > - </button> 
             </div>
         </div>
     )

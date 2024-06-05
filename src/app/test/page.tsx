b5d7b37320093fc3fc6c4c2da39501da77a5c48d
chelo.appventures <chelo.appventures@gmail.com>
2024-06-05 16:34:07 -0300

    fix: test page

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,72 +1,76 @@
-'use client'
-import React, { useState } from "react"
+"use client";
+import React, { useState } from "react";
 
-function FormTest() {
-    const cantPersonas = 3
-
-    const [passengers, setPassengers]:any = useState([])
-    const [name, setName] = useState("")
-    const [email, setEmail] = useState("")
-
-    const passenger = {
-        name: name, 
-        email: email
-    }
-    const handleSubmit = (e: any) => {
-        e.preventDefault()
-        
-
-        console.log(passengers)
-    }
-
-    
-    
-    return(
-        <>
-            <form>
-                { 
-                    Array.from( {length:cantPersonas}, (_,i) => i+1).map((personIndex) => {
-                        return (
-                            <>
-                                <h1><strong>Pasajero {personIndex}</strong></h1>
-                                <label>Nombre</label>
-                                <input type="text" 
-                                    onChange={(e:any) => {
-                                        const newPassenger = {
-                                            ...passenger, 
-                                            name: e.currentTarget.value
-                                        }
-                                        const newList = [...passengers]
-                                        newList[personIndex] = newPassenger
-                                        setPassengers(newList)
-                                        
-                                    }}
-                                />
-                                <label>Email</label>
-                                <input type="text" 
-                                    onChange={(e:any) => {
-                                        const newPassenger = {
-                                            ...passenger, 
-                                            em: e.currentTarget.value
-                                        }
-                                        const newList = [...passengers]
-                                        newList[personIndex] = newPassenger
-                                        setPassengers(newList)
-                                        
-                                    }}
-                                />
-                            </>
-                        )
-
-                    })
-                }
-                
-                
-                <button type="submit" className="bg-green-500 rounded-md text-white font-bold p-2" 
-                onClick={handleSubmit} >Enviar</button>
-            </form>
-        </>
-    )
+interface Passenger {
+  name: string;
+  dni: string;
 }
 
-export default FormTest
+const initialPassengers: Passenger[] = [
+  { name: "p--", dni: "1" },
+  { name: "pp-", dni: "2" },
+  { name: "ppp", dni: "3" },
+];
+
+function FormTest() {
+  const [passengers, setPassengers]: any =
+    useState<Passenger[]>(initialPassengers);
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    console.log(passengers);
+  };
+
+  return (
+    <>
+      <form>
+        {passengers.map((passenger: Passenger, index: number) => {
+          return (
+            <>
+              <h1>
+                <strong>Pasajero {index}</strong>
+              </h1>
+              <label>Nombre</label>
+              <input
+                type="text"
+                onChange={(e: any) => {
+                  const newPassenger = {
+                    ...passenger,
+                    name: e.currentTarget.value,
+                  };
+                  const newList = [...passengers];
+                  newList[index] = newPassenger;
+                  setPassengers(newList);
+                }}
+              />
+              <label>Email</label>
+              <input
+                type="text"
+                onChange={(e: any) => {
+                  const newPassenger = {
+                    ...passenger,
+                    email: e.currentTarget.value,
+                  };
+                  const newList = [...passengers];
+                  newList[index] = newPassenger;
+                  setPassengers(newList);
+                }}
+              />
+            </>
+          );
+        })}
+
+        <button
+          type="submit"
+          className="bg-green-500 rounded-md text-white font-bold p-2"
+          onClick={handleSubmit}
+        >
+          Enviar
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default FormTest;

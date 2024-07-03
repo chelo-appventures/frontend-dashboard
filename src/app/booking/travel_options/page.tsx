4dcdd1297f968b7fc63a51db5675ae2d0569b568
federicoalaniz <fedealaniz@gmail.com>
2024-07-03 11:37:54 -0300

    se corrige contenedor en travel_options

--- src/app/booking/travel_options/page.tsx
+++ src/app/booking/travel_options/page.tsx
@@ -67,7 +67,7 @@ export default function TravelOptions() {
 
   return (
     <>
-      <div className="flex min-h-screen flex-col items-center bg-gray-300 max-h-screen">
+      <div className="flex min-h-screen flex-col items-center bg-gray-300 h-full">
         <div className=" bg-[#F4F4F7] w-[1280px] min-h-full flex flex-col">
           <HeaderAV />
           <OptionHeader
